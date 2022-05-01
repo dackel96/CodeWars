bf182dfc5ad0982faeb53e004bc696c8362395fe
dackel96 <92174873+dackel96@users.noreply.github.com>
2022-05-01 22:23:20 +0300

    stepOne

---
+++ CreatePhoneNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakeItHAppen
+{
+    public class CreatePhoneNumbers
+    {
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            int[] firstThre = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                firstThre[i] = numbers[i];
+            }
+            int[] secondThree = new int[3];
+            int counter = 3;
+            for (int i = 0; i < 3; i++)
+            {
+                secondThree[i] = numbers[counter];
+                counter++;
+            }
+            int[] lastDigits = new int[4];
+            int counterForLast = 6;
+            for (int i = 0; i < 4; i++)
+            {
+                lastDigits[i] = numbers[counterForLast];
+                counterForLast++;
+            }
+            string phoneNumber = $"({string.Join("", firstThre)}) {string.Join("", secondThree)}-{string.Join("", lastDigits)}";
+            return phoneNumber;
+        }
+    }
+}
---
+++ SplitString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakeItHAppen
+{
+    class SplitString
+    {
+        public static string[] Solution(string str)
+        {
+            // 'abc' =>  ['ab', 'c_']
+            // *'abcdef' => ['ab', 'cd', 'ef']
+            if (str.Length % 2 == 1)
+            {
+                str = $"{str}_";
+            }
+            string[] result = new string[str.Length / 2];
+            char[] charArr = str.ToCharArray();
+            int index = 0;
+            for (int i = 0; i < charArr.Length; i += 2)
+            {
+                result[index] = $"{charArr[i]}{charArr[i + 1]}";
+                index++;
+            }
+            return result;
+        }
+    }
+}
